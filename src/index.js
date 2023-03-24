a043ee1d587c43e514a9fe95eb7246146099a2a9
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-03-24 02:26:48 +0200

    add fetchCountries/createListCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector("#search-box"),
+    listCountries: document.querySelector(".country-list"),
+    countryContainer: document.querySelector("country-info")
+}
+
+refs.input.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
+
+function onSearchCountry() {
+    const country = refs.input.value;
+    console.log('country', country);
+
+    if(country === "") {
+        return;
+    };
+    fetchCountries(country)
+    .then(createCountriesList)
+  
+    
+}
+
+function createCountriesList(countries) {
+    const markup = countries
+    .map(({ name, flags }) => {
+      return `
+      <li> 
+        <img src = ${flags.svg} 
+            alt = "${name}" 
+            width = 100>
+        <span>${name.official}</span>
+      </li>`;
+    })
+    .join('');
+  refs.listCountries.insertAdjacentHTML('beforeend',markup); 
+}
